d1fe5664c8f8de8bd6f6bb803ef68842dfdb690e
Jonathan Alvarez <j@jonalvarezz.com>
2020-11-25 21:39:55 +0100

    tiidy up

--- workshop-lazy-loading/src/index.js
+++ workshop-lazy-loading/src/index.js
@@ -1,5 +1,5 @@
 import { registerImage } from "./lazy";
 
+// Cargue las imagenes existentes cuando cargue la página
 const allImages = document.querySelectorAll("img[data-src]");
-
 allImages.forEach(registerImage);
